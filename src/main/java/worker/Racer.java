a4a04216c995c7541440739d571cd98780034fc6
nickl0gist <mykola.horkov@gmail.com>
2023-05-16 14:18:48 +0200

    Behaviours usage. The behavior of the Racer changed in order to split start, running and finished stages into 3 separate methods. Class variables moved to appropriate places inside methods.

--- src/main/java/worker/Racer.java
+++ src/main/java/worker/Racer.java
@@ -23,25 +23,19 @@ public class Racer extends AbstractBehavior<Command> {
     private int averageSpeedAdjustmentFactor;
     private Random random;
 
-    private double currentSpeed = 0;
-    private double currentPosition = 0;
-    private int raceLength;
-    private boolean isFinished;
-    private double currentTime;
 
     private double getMaxSpeed() {
-        return defaultAverageSpeed * (1+((double)averageSpeedAdjustmentFactor / 100));
+        return defaultAverageSpeed * (1 + ((double) averageSpeedAdjustmentFactor / 100));
     }
 
-    private double getDistanceMovedPerSecond() {
+    private double getDistanceMovedPerSecond(double currentSpeed) {
         return currentSpeed * 1000 / 3600;
     }
 
-    private void determineNextSpeed() {
+    private double determineNextSpeed(int raceLength, int currentPosition, double currentSpeed) {
         if (currentPosition < (raceLength / 4)) {
-            currentSpeed = currentSpeed  + (((getMaxSpeed() - currentSpeed) / 10) * random.nextDouble());
-        }
-        else {
+            currentSpeed = currentSpeed + (((getMaxSpeed() - currentSpeed) / 10) * random.nextDouble());
+        } else {
             currentSpeed = currentSpeed * (0.5 + random.nextDouble());
         }
 
@@ -54,33 +48,57 @@ public class Racer extends AbstractBehavior<Command> {
         if (currentPosition > (raceLength / 2) && currentSpeed < getMaxSpeed() / 2) {
             currentSpeed = getMaxSpeed() / 2;
         }
+        return currentSpeed;
     }
 
+    @Override
     public Receive<Command> createReceive() {
+        return beforeRace();
+    }
+
+    public Receive<Command> beforeRace() {
         return newReceiveBuilder()
                 .onMessage(StartCommand.class, message -> {
-                    this.raceLength = message.getRaceLength();
                     this.random = new Random();
                     this.averageSpeedAdjustmentFactor = random.nextInt(30) - 10;
-                    return this;
+                    return race(message.getRaceLength(), 0, false, 0, 0);
                 })
                 .onMessage(PositionCommand.class, message -> {
-                    determineNextSpeed();
-                    currentPosition += getDistanceMovedPerSecond();
-                    if (currentPosition >= raceLength){
-                        currentPosition = raceLength;
-                        if(!isFinished)
-                            currentTime += 1;
-
-                        isFinished = true;
-                    } else {
-                        currentTime += 1;
-                    }
-                    message.getController().tell(new RacerUpdateCommand(getContext().getSelf(), (int)currentPosition, isFinished, currentTime));
-                    return this;
+                    message.getController().tell(new RacerUpdateCommand(getContext().getSelf(), 0, false, 0));
+                    return Behaviors.same();
                 })
                 .build();
     }
 
+    public Receive<Command> race(int raceLength, int currentPosition, boolean isFinished, double currentTime, double currentSpeed) {
+        return newReceiveBuilder().onMessage(PositionCommand.class, message -> {
+            double speed = determineNextSpeed(raceLength, currentPosition, currentSpeed);
+            int newPosition = currentPosition;
+            boolean finished = isFinished;
+            double time = currentTime;
+            newPosition += getDistanceMovedPerSecond(speed);
+            if (newPosition >= raceLength) {
+                newPosition = raceLength;
+                if (!finished)
+                    time++;
 
+                finished = true;
+            } else {
+                time++;
+            }
+            message.getController().tell(new RacerUpdateCommand(getContext().getSelf(), newPosition, finished, time));
+            if (finished) {
+                return finished(raceLength, time);
+            } else {
+                return race(raceLength, newPosition, false, time, speed);
+            }
+        }).build();
+    }
+
+    public Receive<Command> finished(int raceLength, double currentTime) {
+        return newReceiveBuilder().onMessage(PositionCommand.class, message -> {
+            message.getController().tell(new RacerUpdateCommand(getContext().getSelf(), raceLength, true, currentTime));
+            return Behaviors.same();
+        }).build();
+    }
 }
